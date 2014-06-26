adf26d201f865e647b0eedfd9be4a8d0a34290ab
Toby Retallick <toby.retallick@gmail.com>
2014-06-26 22:32:07 +0100

    Patterns class can determine frequency of patterns for existing emails

--- lib/patterns.rb
+++ lib/patterns.rb
@@ -11,9 +11,23 @@ class Patterns
   end
 
   def first_initial_dot_last_name(emails)
-    formatted_emails = remove_domain(emails)
-    { first_initial_dot_last_name: formatted_emails.map { |email| email if email.split('.').first.length == 1 }.length }
+    result = Hash.new
+    result[:first_initial_dot_last_name] = remove_domain(emails).select { |email| email if email.split('.').first.length == 1 }.length
+    result
   end
+
+  def first_name_dot_last_initial(emails)
+    result = Hash.new
+    result[:first_name_dot_last_initial] = remove_domain(emails).select { |email| email if email.split('.').last.length == 1 }.length
+    result
+  end
+
+  def first_initial_dot_last_initial(emails)
+    result = Hash.new
+    result[:first_initial_dot_last_initial] = remove_domain(emails).select { |email| email if email.length == 3 }.length
+    result
+  end
+
 
   def remove_domain(emails)
     emails.map { |k, v| emails[k].split('@').first }
--- spec/patterns_spec.rb
+++ spec/patterns_spec.rb
@@ -14,8 +14,7 @@ describe 'Patterns' do
     matching_emails = lookup.search
     patterns = Patterns.new(matching_emails)
 
-    expect(patterns.first_name_dot_last_name(matching_emails)).to eq({ first_name_dot_last_name: 3} )
-    #expect(patterns.first_initial_dot_last_name(matching_emails)).to eq({first_initial_dot_last_name: 0})
+    expect(patterns.first_name_dot_last_name(matching_emails)).to eq({ first_name_dot_last_name: 3 })
 
   end
 
@@ -25,27 +24,27 @@ describe 'Patterns' do
     matching_emails = lookup.search
     patterns = Patterns.new(matching_emails)
 
-    expect(patterns.first_initial_dot_last_name(matching_emails)).to eq({first_initial_dot_last_name: 2})
+    expect(patterns.first_initial_dot_last_name(matching_emails)).to eq({ first_initial_dot_last_name: 1 })
 
   end
 
-  xit "knows if an existing email matches the pattern: first_name_dot_last_initial" do
+  it "knows if an existing email matches the pattern: first_name_dot_last_initial" do
 
-    lookup = Lookup.new(peter)
+    lookup = Lookup.new(craig)
     matching_emails = lookup.search
     patterns = Patterns.new(matching_emails)
 
-    expect(patterns.first_name_dot_last_initial(matching_emails)).to eq({first_name_dot_last_initial: 1})
+    expect(patterns.first_name_dot_last_initial(matching_emails)).to eq({ first_name_dot_last_initial: 1 })
 
   end
 
-  xit "knows if an existing email matches the pattern: first_initial_dot_last_initial" do
+  it "knows if an existing email matches the pattern: first_initial_dot_last_initial" do
 
-    lookup = Lookup.new(peter)
+    lookup = Lookup.new(steve)
     matching_emails = lookup.search
     patterns = Patterns.new(matching_emails)
 
-    expect(patterns.first_initial_dot_last_initial(matching_emails)).to eq({first_inital_dot_last_initial: 3})
+    expect(patterns.first_initial_dot_last_initial(matching_emails)).to eq({first_initial_dot_last_initial: 1})
 
   end
 
