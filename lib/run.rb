be0973b17aee4f970a113475afdf9dfb29e00253
Toby Retallick <toby.retallick@gmail.com>
2014-06-27 02:19:07 +0100

    Fixed console formatting

--- lib/run.rb
+++ lib/run.rb
@@ -4,12 +4,16 @@ require 'target.rb'
 emails = [Target.new("Peter Wong", "alphasights.com"),
           Target.new("Craig Silverstein", "google.com"),
           Target.new("Steve Wozniak", "apple.com"),
+          Target.new("Barack Obama", "whitehouse.gov")
           ]
 
+puts "\n"
 puts "Email Predictions"
 puts "-----------------"
+puts "\n"
 
 emails.each do |person|
   check = Lookup.new(person)
   puts "#{check.target.name}: #{check.format_email}"
 end
+puts "\n"
